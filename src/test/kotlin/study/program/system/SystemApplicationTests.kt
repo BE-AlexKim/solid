4c0c0b62b354cc1dbfdb983314aa09d4042d9b9c
Developerbabe <118235272+BE-AlexKim@users.noreply.github.com>
2025-02-20 18:47:24 +0900

    init
    created study project

---
+++ src/test/kotlin/study/program/system/SystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.program.system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
